786ea4d48c6bc49d058d955c70e6f985341f1c4b
simonrosengren <simonrosengren123@gmail.com>
2021-12-15 11:39:09 +0100

    4

---
+++ 4/4-1.js
@@ -0,0 +1,14 @@
+const utils = require('../utils');
+const input = utils.inputToStringArray('4/input.txt');
+const calls = input[0].split(',');
+let players = [];
+
+for (let i = 2; i < input.length; i++) {
+    if (input[i] === '') continue;
+    for (let j = 0; j < 5; j++) {
+            
+        i++;
+    }
+}
+
+console.log(calls)
