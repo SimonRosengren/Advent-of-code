88404ba76cee544fdd011de933670750a88f8ead
simonrosengren <simonrosengren123@gmail.com>
2021-12-22 21:36:49 +0100

    9 recursion

--- 9/9.js
+++ 9/9.js
@@ -12,84 +12,77 @@ for (let i = 0; i < input.length; i++) {
       lowpoint = false;
     if (i < input.length - 1 && input[i + 1][j] <= input[i][j])
       lowpoint = false;
-    if (lowpoint) lowPoints.push([i, j])
-    if (lowpoint) riskScore += parseInt(input[i][j]) + 1
+    if (lowpoint) lowPoints.push([i, j]);
+    if (lowpoint) riskScore += parseInt(input[i][j]) + 1;
   }
 }
 
-console.log('Part I: ' + riskScore);
+console.log("Part I: " + riskScore);
 
-const includesPosition = (pos, history) => {
-  for (let i = 0; i < history.length; i++) {
-    if (history[i][0] === pos[0] && history[i][1] === pos[1]) return true;
-  }
-  return false;
+let currentPos = {
+    x: 0,
+    y: 0
 }
-
-const isSamePosition = (pos1, pos2) => {
-  if (pos1[0] === pos2[0] && pos1[1] == pos2[1]) return true;
-  return false;
-}
-
-
 
 let basinMap = [];
 for (let i = 0; i < input.length; i++) {
-  basinMap.push([])
+  basinMap.push([]);
   for (let j = 0; j < input[i].length; j++) {
-    basinMap[i][j] = 0;
+    basinMap[i].push(0);
   }
 }
 
-let walker = {
-  x: 0,
-  y: 0,
-  startX: 0,
-  startY: 0,
-  backTrackCounter: 1,
-  history: [],
-  start: startPos => {
-    walker.history = [];
-    x = startPos[0];
-    y = startPos[1];
-    startX = x;
-    startY = y;
-    walker.history.push([startPos[0], startPos[1]])
-    walker.wander();
-    while (!isSamePosition([walker.x, walker.y], [startX, startY])) { // << bygg finished() som kollar om alla möjliga steg från början redan liggerr i histroy samt att vi är på starten igen
-      walker.wander();
+// const includesPosition = (pos, history) => {
+//   for (let i = 0; i < history.length; i++) {
+//     if (history[i][0] === pos[0] && history[i][1] === pos[1]) return true;
+//   }
+//   return false;
+// };
+
+// const isSamePosition = (pos1, pos2) => {
+//   if (pos1[0] === pos2[0] && pos1[1] == pos2[1]) return true;
+//   return false;
+// };
+
+const isVisited = (pos) => {
+  return !!basinMap[pos.x][pos.y];
+};
+
+const isViableStep = (currentPos, potentialPos) => {
+    if (potentialPos.x < 0 || potentialPos.x >= input.length) return false;
+    if (potentialPos.y < 0 || potentialPos.y >= input[0].length) return false;
+    if (
+      input[potentialPos.x][potentialPos.y] < 9 &&
+      input[potentialPos.x][potentialPos.y] > input[currentPos.x][currentPos.y] &&
+      !isVisited(potentialPos)
+    ) {
+      return true;
     }
-  },
-  wander: () => {
-    if (input[x + 1] && input[x + 1][y] > input[x][y] && !includesPosition([x + 1, y], walker.history)) {
-      x = x + 1;
-      walker.history.push([x, y])
-      walker.backTrackCounter = 1;
-    } else if (input[x - 1] && input[x - 1][y] > input[x][y] && !includesPosition([x - 1, y], walker.history)) {
-      x = x - 1;
-      walker.history.push([x, y])
-      walker.backTrackCounter = 1;
-    } else if (input[x][y + 1] && input[x][y + 1] > input[x][y] && !includesPosition([x, y + 1], walker.history)) {
-      y = y + 1;
-      walker.history.push([x, y])
-      walker.backTrackCounter = 1;
-    } else if (input[x][y - 1] && input[x][y - 1] > input[x][y] && !includesPosition([x, y - 1], walker.history)) {
-      y = y - 1;
-      walker.history.push([x, y])
-      walker.backTrackCounter = 1;
+    return false;
+};
+  
+const move = (newPos, id) => {
+    currentPos.x = newPos.x;
+    currentPos.y = newPos.y;
+    basinMap[currentPos.x][currentPos.y] = id;
+
+    if (isViableStep(currentPos, { x: currentPos.x + 1, y: currentPos.y })) {
+        move({ x: currentPos.x + 1, y: currentPos.y }, id);
+    } else if (isViableStep(currentPos, { x: currentPos.x - 1, y: currentPos.y })) {
+        move({ x: currentPos.x - 1, y: currentPos.y }, id);
+    } else if (isViableStep(currentPos, { x: currentPos.x, y: currentPos.y + 1 })) {
+        move({ x: currentPos.x, y: currentPos.y + 1 }, id);
+    } else if (isViableStep(currentPos, { x: currentPos.x, y: currentPos.y - 1 })) {
+        move({ x: currentPos.x, y: currentPos.y - 1 }, id);
     } else {
-      walker.x = walker.history[walker.history.length - walker.backTrackCounter][0];
-      walker.y = walker.history[walker.history.length - walker.backTrackCounter][1];
-      walker.backTrackCounter++;
+        return;
     }
-  }
-}
+};
 
 for (let i = 0; i < lowPoints.length; i++) {
-  walker.start(lowPoints[i]);
-  for (let j = 0; j < walker.history.length; j++) {
-    basinMap[walker.history[j][0]][walker.history[j][1]] = i;
-    
-  }
+    move({ x: lowPoints[i][0], y: lowPoints[i][1] }, i + 1);
 }
-console.log(lowPoints)
+
+console.table(input);
+console.table(basinMap);
+console.log(lowPoints);
