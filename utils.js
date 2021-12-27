e88f4bcd7422edb20d33dc5e3404c702fa12a88f
simonrosengren <simonrosengren123@gmail.com>
2021-12-27 16:12:47 +0100

    handle line break for windows and mac

--- utils.js
+++ utils.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const inputToStringArray = filename => {
     let data = fs.readFileSync(filename, 'utf8');
-    data = data.split('\r\n');
+    data = data.replace('\r', '').split('\n');
     return data;
 }
 
