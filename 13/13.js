3b873744a0d2811da754adccd620e746282a6d18
simonrosengren <simonrosengren123@gmail.com>
2021-12-29 23:35:11 +0100

    13

--- 13/13.js
+++ 13/13.js
@@ -3,16 +3,30 @@ const input = inputToStringArray("13/input.txt");
 let grid = [];
 
 let maxX = 0, maxY = 0;
+let foldsStartsAt = 0;
 for (let i = 0; i < input.length; i++) {
-    if (isNaN(input[i][0])) break;
+    if (isNaN(input[i][0])) {
+        foldsStartsAt = i + 1;
+        break;
+    }
     let x = parseInt(input[i].split(',')[0]);
     let y = parseInt(input[i].split(',')[1]);
     if (x > maxX) maxX = x;
     if (y > maxY) maxY = y;
 }
 
+const partI = () => {
+    let counter = 0;
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            if (grid[i][j] === '#') counter++;
+        }
+    }
+    return counter;
+}
+
 const foldAlongY = y => {
-    for (let i = grid.length - 1; i > y; i--) {
+    for (let i = grid.length - 1; i >= y; i--) {
         let row = grid.pop();
         for (let j = 0; j < row.length; j++) {
             if (row[j] === '#') grid[y - (i - y)][j] = row[j];
@@ -20,10 +34,19 @@ const foldAlongY = y => {
     }
 }
 
+const foldAlongX = x => {
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = grid[i].length - 1; j >= x; j--) {
+            let value = grid[i].pop();
+            if (value === '#') grid[i][x - (j - x)] = value;
+        }   
+    }
+}
+
 for (let y = 0; y < maxY + 1; y++) {
     grid.push([]);
     for (let x = 0; x < maxX + 1; x++) {
-        grid[y][x] = '.';
+        grid[y][x] = ' ';
     }
 }
 
@@ -34,11 +57,17 @@ for (let i = 0; i < input.length; i++) {
     grid[y][x] = '#';
 }
 
-foldAlongY(7)
+for (let i = foldsStartsAt; i < input.length; i++) {
+    let instruction = input[i].split('fold along ')[1];
+    let split = instruction.split('=');
+    split[0] === 'y' ? foldAlongY(split[1]) : foldAlongX(split[1])
+    if (i === foldsStartsAt) console.log('ptI: ' + partI());
+}
+
 for (let i = 0; i < grid.length; i++) {
     let row = '';
     for (let j = 0; j < grid[i].length; j++) {
-        row += ' ' + grid[i][j] + ' ';
+        row += '' + grid[i][j] + '';
     }
     console.log(row);
 }
