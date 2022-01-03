5c5a50c027b54089abf1ff30af5d992e9ead62b1
simonrosengren <simonrosengren123@gmail.com>
2022-01-04 00:25:19 +0100

    15

--- 15/15.js
+++ 15/15.js
@@ -1,70 +1,69 @@
 const { inputToGrid, isWithinGridBounds } = require("../utils");
 const input = inputToGrid("15/input.txt");
+const grid = []
 let prioQueue = [];
-let path = [];
 
 let currentPos = {
     x: 0,
     y: 0
 }
-for (let i = 0; i < input.length; i++) {
-    for (let j = 0; j < input[i].length; j++) {
-        input[i][j] = {
-            pos: {
-                x: j,
-                y: i
-            },
-            weight: parseInt(input[i][j]),
-            cost: Infinity,
-            via: {x: 0, y: 0},
-            visited: false
+
+
+for (let k = 0; k < 5; k++) {
+    for (let i = 0; i < input.length; i++) {
+        grid.push([])
+        for (let h = 0; h < 5; h++) {
+            for (let j = 0; j < input[i].length; j++) {
+                grid[i + (k * input.length)].push({
+                    pos: {
+                        x: j + (h * input[0].length),
+                        y: i + (k * input.length)
+                    },
+                    weight: (parseInt(input[i][j]) + (h + k)) > 9 ? ((parseInt(input[i][j]) + (h + k)) % 10) + 1 : parseInt(input[i][j]) + h + k,
+                    cost: Infinity,
+                    via: { x: 0, y: 0 },
+                    visited: false
+                })
+                prioQueue.push(grid[i + (k * input.length)][j + (h * input[0].length)])
+            }
         }
-        if (!(i === 0 && j === 0)) prioQueue.push(input[i][j])
-    }   
+    }
 }
 
 const sortPrioQueue = () => {
     prioQueue.sort((a, b) => b.cost - a.cost);
 }
-
 const explore = () => {
+
     let y = currentPos.y, x = currentPos.x;
 
-    if (isWithinGridBounds(input, {x: x + 1, y}) && input[y][x + 1].weight + input[y][x].cost < input[y][x + 1].cost) {
-        input[y][x + 1].cost = input[y][x + 1].weight + input[y][x].cost;
-        input[y][x + 1].via = {x, y};
-    } 
-    if (isWithinGridBounds(input, {x: x - 1, y}) && input[y][x - 1].weight + input[y][x].cost < input[y][x - 1].cost) {
-        input[y][x - 1].cost = input[y][x - 1].weight + input[y][x].cost;
-        input[y][x - 1].via = {x, y};
-    } 
-    if (isWithinGridBounds(input, {x, y: y + 1}) && input[y + 1][x].weight + input[y][x].cost < input[y + 1][x].cost) {
-        input[y + 1][x].cost = input[y + 1][x].weight + input[y][x].cost;
-        input[y + 1][x].via = {x, y};
-    } 
-    if (isWithinGridBounds(input, {x, y: y - 1}) && input[y - 1][x].weight + input[y][x].cost < input[y - 1][x].cost) {
-        input[y - 1][x].cost = input[y - 1][x].weight + input[y][x].cost;
-        input[y - 1][x].via = {x, y};
-    } 
+    if (isWithinGridBounds(grid, { x: x + 1, y }) && grid[y][x + 1].weight + grid[y][x].cost < grid[y][x + 1].cost) {
+        grid[y][x + 1].cost = grid[y][x + 1].weight + grid[y][x].cost;
+        grid[y][x + 1].via = { x, y };
+    }
+    if (isWithinGridBounds(grid, { x: x - 1, y }) && grid[y][x - 1].weight + grid[y][x].cost < grid[y][x - 1].cost) {
+        grid[y][x - 1].cost = grid[y][x - 1].weight + grid[y][x].cost;
+        grid[y][x - 1].via = { x, y };
+    }
+    if (isWithinGridBounds(grid, { x, y: y + 1 }) && grid[y + 1][x].weight + grid[y][x].cost < grid[y + 1][x].cost) {
+        grid[y + 1][x].cost = grid[y + 1][x].weight + grid[y][x].cost;
+        grid[y + 1][x].via = { x, y };
+    }
+    if (isWithinGridBounds(grid, { x, y: y - 1 }) && grid[y - 1][x].weight + grid[y][x].cost < grid[y - 1][x].cost) {
+        grid[y - 1][x].cost = grid[y - 1][x].weight + grid[y][x].cost;
+        grid[y - 1][x].via = { x, y };
+    }
     sortPrioQueue();
     let nextStep = prioQueue.pop();
     currentPos.x = nextStep.pos.x;
     currentPos.y = nextStep.pos.y;
-    path.push(nextStep);
 }
 
-input[currentPos.y][currentPos.x].cost = 0; // Set start to weight 0
+grid[currentPos.y][currentPos.x].cost = 0; // Lazy fix to block gong back to start
 
 
-while (!(currentPos.x === input[0].length - 1 && currentPos.y === input.length - 1)) {
-
+while (!(currentPos.x === grid[0].length - 1 && currentPos.y === grid.length - 1)) {
     explore();
-    // Same for all other directions
-    // Add them all to prio queue sorted
-    // Add next in line to via of currentPos
-    // Go to the next pos in line (cheapest)
-    // Repeat this until end is find. 
-    // Step backwards through "via" to get shortest path
 }
 
-console.log('das')
+console.log(grid[grid.length - 1][grid[0].length - 1].cost)
